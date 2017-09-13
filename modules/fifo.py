e8c97729ca59835cd6f9d4f26c406d059a47ef53
abhalverson <adam.b.halverson@gmail.com>
2017-09-13 18:15:37 -0400

    Added four-button video functionality

--- modules/fifo.py
+++ modules/fifo.py
@@ -60,22 +60,26 @@ def gpio_handler_4_button(settings, **kwargs) -> bool:
     try:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-        GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
         while True:
             if not GPIO.input(17):
-                print("Button 17: has been pressed!")
+                passthrough(settings, command="pause")
+                print("Pause Button Pressed.")
+
+            if not GPIO.input(22):
+                passthrough(settings, command="seek 1 0")
+                print("Fwd x10 Button Pressed.")
 
             if not GPIO.input(23):
-                print("Button 23: has been pressed!")
-
-            if not GPIO.input(26):
-                print("Button 26: has been pressed!")
+                passthrough(settings, command="seek -1 0")
+                print("Rew x10 Button Pressed.")
 
             if not GPIO.input(27):
-                print("Button 27: has been pressed!")
+                print("Exit Button Pressed.")
+                passthrough(settings, command="exit")
 
             time.sleep(0.2)
     finally:
@@ -130,5 +134,6 @@ MODULE = {
     "exit": exit,
     "buttons": gpio_handler,
     "one_button": gpio_handler_1_button,
+    "four_button": gpio_handler_4_button,
     "": run,
 }
