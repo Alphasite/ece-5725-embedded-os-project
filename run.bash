2866676886b088e946b26dbcd2f36d49998be9c4
abhalverson <adam.b.halverson@gmail.com>
2017-09-20 16:44:42 -0400

    fix silly error with run file.

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
-mplayer -m sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=/home/pi/fifos/video_fifo -v sdl -framedrop /home/pi/bigbuckbunny320p.mp4 &
+sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -input file=/home/pi/fifos/video_fifo -v sdl -framedrop /home/pi/bigbuckbunny320p.mp4 &
 source ../venv/bin/activate
 python run.py fifo.buttons
