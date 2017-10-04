c6689e18aba4e9e8103d81317f6caa2a3f4b63e6
abhalverson <adam.b.halverson@gmail.com>
2017-10-04 16:29:57 -0400

    Module addition.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,11 @@
 from modules import fifo as __fifo
 from modules import lab1 as __lab1
 from modules import lab2 as __lab2
+from modules import lab3 as __lab3
 
 MODULE = {
     "fifo": __fifo.MODULE,
     "lab1": __lab2.MODULE,
     "lab2": __lab2.MODULE,
+    "lab3": __lab2.MODULE,
 }
