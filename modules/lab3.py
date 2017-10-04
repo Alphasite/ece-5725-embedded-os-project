b2dd9a3cabe2c749a8643503ace564d622e4d1cd
abhalverson <adam.b.halverson@gmail.com>
2017-10-04 17:47:21 -0400

    fixed pin number.

--- modules/lab3.py
+++ modules/lab3.py
@@ -48,7 +48,7 @@ class Servo:
         self.pulse_width = Servo.zero_pulse_width
 
         GPIO.setup(servo_pin, GPIO.OUT, initial=GPIO.LOW)
-        self.pwm = GPIO.PWM(26, self.frequency)
+        self.pwm = GPIO.PWM(servo_pin, self.frequency)
         self.pwm.start(self.duty_cycle)
 
     def set_pwm(self):
@@ -129,8 +129,8 @@ def pwm_calibrate(settings, **kwargs):
 def servo_control(settings,**kwargs):
     GPIO.setmode(GPIO.BCM)
 
-    servo1 = Servo(26)
-    servo2 = Servo(19)
+    servo1 = Servo(19)
+    servo2 = Servo(26)
 
     servo1.speed = 0
     servo2.speed = 0
