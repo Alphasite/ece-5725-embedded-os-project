fa78f0fa87c95959e65e9f42665f5a58dfc47782
Nishad <nishadmathur@gmail.com>
2017-10-18 17:25:30 -0400

    added lab 4 module.

--- modules/__init__.py
+++ modules/__init__.py
@@ -7,10 +7,12 @@ from modules import fifo as __fifo
 from modules import lab1 as __lab1
 from modules import lab2 as __lab2
 from modules import lab3 as __lab3
+from modules import lab4 as __lab4
 
 MODULE = {
     "fifo": __fifo.MODULE,
     "lab1": __lab2.MODULE,
     "lab2": __lab2.MODULE,
     "lab3": __lab3.MODULE,
+    "lab4": __lab4.MODULE,
 }
